39a49286bc4f7614424b825e5e5546ec36140f99
joselws <josel.wilhelms@gmail.com>
2022-03-06 11:29:16 -0400

    heroku deployment config

--- commerce/settings.py
+++ commerce/settings.py
@@ -76,8 +76,14 @@ WSGI_APPLICATION = 'commerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd5did2uhskv7r1',
+        'HOST': 'ec2-52-44-209-165.compute-1.amazonaws.com',
+        'USER': 'rgcafjzatlunfg',
+        'PASSWORD': '0075d0a404b618ec1516aeab31e9972317b848137f7eed3d416ed4f3e5bc1857',
+        'PORT': '5432'
     }
 }
 
